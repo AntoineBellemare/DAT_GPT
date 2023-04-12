980a7fe1c2d55ccbfb689e7b1e33c86f86bd02d4
Antoine Bellemare <antoine.bellemare9@gmail.com>
2023-04-12 04:06:09 -0400

    updated figures

--- scripts/multiple_test.py
+++ scripts/multiple_test.py
@@ -5,38 +5,59 @@ import matplotlib.pyplot as plt
 from scipy.stats import ttest_ind
 from statsmodels.stats.multitest import multipletests
 
-def create_heatmap(mean_conf, variable, tvals_table, pvals_table, pal, order):
+def create_heatmap(mean_conf, variable, tvals_table, pvals_table, pal, order, color_order=None, xlim=(60, 90), save=None, large=10):
+    if color_order is not None:
+        pal = color_order
     # Generate a mask for the upper triangle
     mask = np.triu(np.ones_like(tvals_table, dtype=bool))
 
     # Set up the matplotlib figure
-    fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(25, len(mean_conf[variable].unique())*1.2))
-    sns.barplot(y=variable, x='mean', data=mean_conf, ax=axs[0], palette=pal, order=order)
-    axs[0].set_ylabel('Score')
-    axs[0].set_title('Mean Scores with Confidence Intervals')
+    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(large, len(mean_conf[variable].unique())*1.2))
+    
+    if color_order is not None:
+        pal = dict(zip(order, color_order))
 
-    # Generate the heat map for t-values
-    sns.heatmap(tvals_table, mask=mask, annot=True, fmt=".2f", cmap="magma",
-                cbar_kws={'label': 't-value'}, ax=axs[1])
-    axs[1].set_title('Contrasts - t-values')
+    bar_plot = sns.barplot(x=variable, y='mean', data=mean_conf, ax=axs[0], palette=pal, order=order, ci=None)
+    
+    # Add error bars to the barplot
+    for i, model in enumerate(order):
+        model_data = mean_conf[mean_conf[variable] == model]
+        error = model_data['std'].values[0]
+        axs[0].errorbar(x=i, y=model_data['mean'].values[0], yerr=error, fmt='none', color='black', capsize=5)
+        
+    axs[0].set_xlabel('Score')
+    #axs[0].set_xticklabels(order, rotation=20)
+    axs[0].set_title('Mean Scores with Standard Deviations')
 
-    # Generate the heat map for p-values
+    # Set y-axis ticks manually
+    yticks = np.linspace(xlim[0], xlim[1], num=6)  # Change num to the desired number of ticks
+    bar_plot.set_yticks(yticks)
+    bar_plot.set_ylim(xlim)
+
+    # Generate the combined heat map for t-values and p-values
+    tvals_annot = tvals_table.applymap(lambda x: f"\n{x:.2f}")
     pval_stars = pvals_table.applymap(lambda x: '***' if x < 0.001 else '**' if x < 0.01 else '*' if x < 0.05 else '')
-    norm = plt.Normalize(vmin=np.nanmin(pvals_table.values), vmax=np.nanmax(pvals_table.values))
-    pal_p = sns.color_palette("magma_r", as_cmap=True)
-    sns.heatmap(pd.DataFrame(norm(pvals_table.values)), mask=mask, annot=pval_stars, fmt="", cmap=pal_p,
-                cbar_kws={'label': 'p-value'}, ax=axs[2],norm=norm)
-    axs[2].set_title('Contrasts - p-values')
+    combined_annot = pval_stars + tvals_annot
+    max_tval = np.abs(tvals_table.values[~mask]).max()
+    sns.heatmap(tvals_table, mask=mask, annot=combined_annot, fmt="", cmap="coolwarm",
+            cbar_kws={'label': 't-value'}, ax=axs[1], vmin=-max_tval, vmax=max_tval)
+
+    axs[1].set_title('Contrasts - t-values and p-values')
+    #axs[1].set_xticklabels(order, rotation=20)
 
     plt.tight_layout()
+    if save is not None:
+        plt.savefig('{}.png'.format(save), dpi=300)
     plt.show()
+
+
 
 
 def analyze_results(results_df, variable, order):
     variables = order
     n_variables = len(results_df[variable].unique())
     # Calculate mean and confidence interval for each model's distribution
-    mean_conf = results_df.groupby(variable)['Score'].agg(['mean', 'sem'])
+    mean_conf = results_df.groupby(variable)['Score'].agg(['mean', 'sem', 'std'])
     mean_conf['ci_low'] = mean_conf['mean'] - 1.96 * mean_conf['sem']
     mean_conf['ci_high'] = mean_conf['mean'] + 1.96 * mean_conf['sem']
     mean_conf.reset_index(inplace=True)
