f93bd58444bc1e494124bffb3fcf2c8c65f6b848
Philipp Thölke <philipp.thoelke@posteo.de>
2023-05-12 16:00:45 -0700

    Add comment

--- scripts/api_call_dat_vicuna.py
+++ scripts/api_call_dat_vicuna.py
@@ -27,6 +27,7 @@ strategies = {
 
 from llama_cpp import Llama
 
+# download the model file from https://huggingface.co/TheBloke/gpt4-x-vicuna-13B-GGML/resolve/main/gpt4-x-vicuna-13B.ggml.q8_0.bin before running this
 llm = Llama(model_path="models/gpt4-x-vicuna-13B-GGML/gpt4-x-vicuna-13B.ggml.q8_0.bin", use_mmap=False)
 
 def generate_response(dat_prompt, temp):
