70063e745f4b7f568a8330decd53e3cd44dabc8c
AntoineBellemare <antoine.bellemare9@gmail.com>
2023-05-11 23:15:18 -0700

    new figures

--- scripts/multiple_test.py
+++ scripts/multiple_test.py
@@ -7,98 +7,15 @@ from scipy.stats import ttest_ind
 from statsmodels.stats.multitest import multipletests
 
 
-def create_heatmap(
-    mean_conf,
-    variable,
-    tvals_table,
-    pvals_table,
-    pal,
-    order,
-    color_order=None,
-    xlim=(60, 90),
-    save=None,
-    large=10,
-):
-    # Generate a mask for the upper triangle
-    mask = np.triu(np.ones_like(tvals_table, dtype=bool))
-
-    # Set up the matplotlib figure
-    fig, axs = plt.subplots(
-        nrows=1, ncols=2, figsize=(large, len(mean_conf[variable].unique()) * 1.2)
-    )
-
-    if color_order is not None:
-        pal = dict(zip(order, color_order))
-
-    bar_plot = sns.barplot(
-        x=variable,
-        y="mean",
-        data=mean_conf,
-        ax=axs[0],
-        palette=pal,
-        order=order,
-        errorbar=None,
-    )
-
-    # Add error bars to the barplot
-    for i, model in enumerate(order):
-        model_data = mean_conf[mean_conf[variable] == model]
-        error = model_data["std"].values[0]
-        axs[0].errorbar(
-            x=i,
-            y=model_data["mean"].values[0],
-            yerr=error,
-            fmt="none",
-            color="black",
-            capsize=5,
-        )
-
-    axs[0].set_xlabel("Score")
-    # axs[0].set_xticklabels(order, rotation=20)
-    axs[0].set_title("Mean Scores with Standard Deviations")
-
-    # Set y-axis ticks manually
-    yticks = np.linspace(
-        xlim[0], xlim[1], num=6
-    )  # Change num to the desired number of ticks
-    bar_plot.set_yticks(yticks)
-    bar_plot.set_ylim(xlim)
-
-    # Generate the combined heat map for t-values and p-values
-    tvals_annot = tvals_table.applymap(lambda x: f"\n{x:.2f}")
-    pval_stars = pvals_table.applymap(
-        lambda x: "***" if x < 0.001 else "**" if x < 0.01 else "*" if x < 0.05 else ""
-    )
-    combined_annot = pval_stars + tvals_annot
-    max_tval = np.abs(tvals_table.values[~mask]).max()
-    sns.heatmap(
-        tvals_table,
-        mask=mask,
-        annot=combined_annot,
-        fmt="",
-        cmap="coolwarm",
-        cbar_kws={"label": "t-value"},
-        ax=axs[1],
-        vmin=-max_tval,
-        vmax=max_tval,
-    )
-
-    axs[1].set_title("Contrasts - t-values and p-values")
-    # axs[1].set_xticklabels(order, rotation=20)
-
-    plt.tight_layout()
-    if save is not None:
-        plt.savefig(f"{save}.png", dpi=300)
-    plt.show()
-
-
 def analyze_results(results_df, variable, order):
     variables = order
     n_variables = len(results_df[variable].unique())
     # Calculate mean and confidence interval for each model's distribution
-    mean_conf = results_df.groupby(variable)["Score"].agg(["mean", "sem", "std"])
+    mean_conf = results_df.groupby(variable)["Score"].agg(["mean", "median", "sem", "std"])
     mean_conf["ci_low"] = mean_conf["mean"] - 1.96 * mean_conf["sem"]
     mean_conf["ci_high"] = mean_conf["mean"] + 1.96 * mean_conf["sem"]
+    mean_conf["1std"] = mean_conf["mean"] + mean_conf["std"]
+    mean_conf["-1std"] = mean_conf["mean"] - mean_conf["std"]
     mean_conf.reset_index(inplace=True)
 
     # Contrast all models using nonparametric t-tests
@@ -139,6 +56,130 @@ def analyze_results(results_df, variable, order):
             tvals_table.loc[model2, model1] = tvals[j, i]
 
     return mean_conf, pvals_table, tvals_table
+
+
+def create_heatmap(
+    mean_conf,
+    variable,
+    tvals_table,
+    pvals_table,
+    pal,
+    order,
+    color_order=None,
+    xlim=(60, 90),
+    save=None,
+    large=(10, 5),
+    rotation=90,
+    axis_name = 'Mean',
+    title_name = 'Mean DAT score',
+):
+    # Generate a mask for the upper triangle
+    mask = np.triu(np.ones_like(tvals_table, dtype=bool))
+
+    # Set up the matplotlib figure
+    fig, axs = plt.subplots(
+        nrows=1, ncols=2, figsize=large
+    )
+
+    if color_order is not None:
+        pal = dict(zip(order, color_order))
+
+    bar_plot = sns.barplot(
+        x=variable,
+        y="mean",
+        data=mean_conf,
+        ax=axs[0],
+        palette=pal,
+        order=order,
+        errorbar=None,
+    )
+    bar_plot.set_xticklabels(order, rotation=rotation)
+    # Add error bars to the barplot
+    for i, model in enumerate(order):
+        model_data = mean_conf[mean_conf[variable] == model]
+        error = model_data["sem"].values[0]
+        axs[0].errorbar(
+            x=i,
+            y=model_data["mean"].values[0],
+            yerr=error,
+            fmt="none",
+            color="black",
+            capsize=5,
+        )
+    # remove x-axis title
+    axs[0].set_xlabel("")
+    axs[0].set_ylabel(axis_name)
+    # axs[0].set_xticklabels(order, rotation=20)
+    axs[0].set_title(title_name)
+
+    # Set y-axis ticks manually
+    yticks = np.linspace(
+        xlim[0], xlim[1], num=6
+    )  # Change num to the desired number of ticks
+    bar_plot.set_yticks(yticks)
+    bar_plot.set_ylim(xlim)
+    # set size of all fonts
+    bar_plot.tick_params(labelsize=16)
+    # set size of title
+    bar_plot.set_title(bar_plot.get_title(), fontsize=18, y=1.05)
+    # set size of x-axis label
+    bar_plot.set_xlabel(bar_plot.get_xlabel(), fontsize=18)
+    # set size of y-axis label
+    bar_plot.set_ylabel(bar_plot.get_ylabel(), fontsize=18)
+    # remove box around plot
+    sns.despine(ax=axs[0], top=True, right=True, left=False, bottom=False)
+    # Generate the combined heat map for t-values and p-values
+    tvals_annot = tvals_table.applymap(lambda x: f"\n{x:.2f}")
+    pval_stars = pvals_table.applymap(
+        lambda x: "***" if x < 0.001 else "**" if x < 0.01 else "*" if x < 0.05 else ""
+    )
+    combined_annot = pval_stars + tvals_annot
+    max_tval = np.abs(tvals_table.values[~mask]).max()
+    # remove the first row and last column from the mask and the table
+    mask = mask[1:, :-1]
+    tvals_table = tvals_table.iloc[1:, :-1]
+    combined_annot = combined_annot.iloc[1:, :-1]
+    sns.heatmap(
+        tvals_table,
+        mask=mask,
+        annot=combined_annot,
+        fmt="",
+        cmap="coolwarm",
+        cbar_kws={"label": "t-value"},
+        ax=axs[1],
+        vmin=-max_tval,
+        vmax=max_tval,
+    )
+    # change label size
+    axs[1].tick_params(labelsize=16)
+    # change title size
+    axs[1].set_title("Pairwise contrasts")
+    # set y label rotation
+    axs[1].set_yticklabels(axs[1].get_yticklabels(), rotation=0)
+    # set axis title size
+    axs[1].set_xlabel(axs[1].get_xlabel(), fontsize=14)
+    axs[1].set_ylabel(axs[1].get_ylabel(), fontsize=14)
+    # set title size
+    axs[1].set_title(axs[1].get_title(), fontsize=18, y=1.05)
+    # change rotation of x-axis labels
+    axs[1].set_xticklabels(axs[1].get_xticklabels(), rotation=rotation)
+    # change heatmap colorbar label size
+    colorbar = axs[1].collections[0].colorbar
+    colorbar.ax.tick_params(labelsize=12)
+    colorbar.set_label("t-values", fontsize=14)
+    
+
+    
+    
+    # axs[1].set_xticklabels(order, rotation=20)
+    # remove axis titles
+    axs[1].set_xlabel("")
+    axs[1].set_ylabel("")
+
+    plt.tight_layout()
+    if save is not None:
+        plt.savefig(f"{save}.png", dpi=300)
+    plt.show()
 
 
 def most_common_words(words_list, n):
