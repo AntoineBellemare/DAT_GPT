1defd81c713379ae07e7c7c2c8c3b4f4781863f7
Philipp Thölke <philipp.thoelke@posteo.de>
2023-05-25 12:51:54 -0700

    Add stop token

--- scripts/api_call_vicuna_stories.py
+++ scripts/api_call_vicuna_stories.py
@@ -41,10 +41,9 @@ llm = Llama(model_path="models/gpt4-x-vicuna-13B-GGML/gpt4-x-vicuna-13B.ggml.q8_
 
 def generate_response(text, temp):
     prompt = f"### Instruction: {text}\n### Response: "
-    output = llm(prompt, max_tokens=1000, temperature=temp)
+    output = llm(prompt, max_tokens=1000, temperature=temp, stop=["###"])
     llm.reset()
     response = output["choices"][0]["text"].strip()
-    print(response)
     return response
 
 @click.command()
